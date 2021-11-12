dab971b37774cec05ebb2caf8f995399a0b9059b
이찬호 <0407chan@naver.com>
2021-11-13 01:34:47 +0900

    [1] 앱 스타일 추가

---
+++ src/appStyle.tsx
@@ -0,0 +1,35 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #282c34;
+  width: 100%;
+  height: 100%;
+`
+export const Header = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #282c34;
+  width: 100%;
+  height: 200px;
+`
+export const Body = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #282c34;
+  width: 100%;
+  height: 100%;
+`
+
+export const Text = styled.span`
+  color: #eeeeee;
+  font-size: 18px;
+  font-weight: bold;
+`
