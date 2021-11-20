ed55ef21262cbf44800ed802349e3ed0316a1869
이찬호 <0407chan@naver.com>
2021-11-20 19:27:45 +0900

    데미지 훅 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,45 @@
 import Button from 'antd/lib/button'
+import InputNumber from 'antd/lib/input-number'
 import Radio from 'antd/lib/radio'
 import React, { useEffect, useState } from 'react'
 import { UseQueryResult } from 'react-query'
-import { useGetDemageSkin, useGetDemageSkinAll } from './api/demage-skin'
+import { useGetDemageSkinAll, useGetWzImage } from './api/demage-skin'
 import * as S from './appStyle'
 import Horizontal from './components/Horizontal'
+import useDamage from './hooks/useDemage'
 import { GetDemageSkinResponse, SkinType } from './type/demage-skin'
 
 const skinTypeOptions = [
-  { label: '일반', value: 'NoRed1' },
-  { label: '크리티컬', value: 'NoCri1' }
+  { label: '일반0', value: 'NoRed0' },
+  { label: '일반1', value: 'NoRed1' },
+  { label: '크리티컬0', value: 'NoCri0' },
+  { label: '크리티컬1', value: 'NoCri1' },
+  { label: '뭐야', value: 'NoRed3' }
 ]
 const App: React.FC = () => {
   const [skinId, setSkinId] = useState<number>(200)
   const [skinType, setSkinType] = useState<SkinType>('NoCri1')
 
   const demageAll = useGetDemageSkinAll({ skinId, skinType })
-  const demage0 = useGetDemageSkin({ skinId, skinType, skinNumber: '0' })
-  const demage1 = useGetDemageSkin({ skinId, skinType, skinNumber: '1' })
-  const demage2 = useGetDemageSkin({ skinId, skinType, skinNumber: '2' })
-  const demage3 = useGetDemageSkin({ skinId, skinType, skinNumber: '3' })
-  const demage4 = useGetDemageSkin({ skinId, skinType, skinNumber: '4' })
-  const demage5 = useGetDemageSkin({ skinId, skinType, skinNumber: '5' })
-  const demage6 = useGetDemageSkin({ skinId, skinType, skinNumber: '6' })
-  const demage7 = useGetDemageSkin({ skinId, skinType, skinNumber: '7' })
-  const demage8 = useGetDemageSkin({ skinId, skinType, skinNumber: '8' })
-  const demage9 = useGetDemageSkin({ skinId, skinType, skinNumber: '9' })
-  const criEffect = useGetDemageSkin({
-    skinId,
-    skinType,
-    skinNumber: 'Effect3'
-  })
-  const numberSpace = useGetDemageSkin({
-    skinId,
-    skinType,
-    skinNumber: 'numberSpace'
-  })
+  const {
+    Miss,
+    criEffect,
+    demage0,
+    demage1,
+    demage2,
+    demage3,
+    demage4,
+    demage5,
+    demage6,
+    demage7,
+    demage8,
+    demage9,
+    guard,
+    numberSpace,
+    resist
+  } = useDamage({ skinId, skinType })
 
+  const star = useGetWzImage()
   useEffect(() => {
     console.log(demageAll.data?.children)
   }, [demageAll.data])
@@ -75,6 +78,7 @@ const App: React.FC = () => {
           -
         </Button>
         <S.Text>{skinId}</S.Text>
+        <InputNumber value={skinId} onChange={(value) => setSkinId(value)} />
         <Button onClick={() => setSkinId(skinId + 1)}>+</Button>
       </Horizontal>
       <Horizontal style={{ justifyContent: 'center' }}>
@@ -106,6 +110,11 @@ const App: React.FC = () => {
           {renderDemage(demage7)}
           {renderDemage(demage8)}
           {renderDemage(demage9)}
+        </Horizontal>
+        <Horizontal style={{ justifyContent: 'center' }}>
+          {renderDemage(guard)}
+          {renderDemage(Miss)}
+          {renderDemage(resist)}
         </Horizontal>
       </S.Body>
     </S.Container>
