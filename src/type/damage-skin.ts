235fa79fa332035770c843d44bc1a05a2501c361
이찬호 <0407chan@naver.com>
2021-11-21 22:34:13 +0900

    오타수정 demage-> damage

--- src/type/damage-skin.ts
+++ src/type/damage-skin.ts
@@ -1,10 +1,10 @@
 export type SkinType = 'NoCri0' | 'NoCri1' | 'NoRed0' | 'NoRed1'
-export type GetDemageSkinQuery = {
+export type GetDamageSkinQuery = {
   skinId?: number
   skinType: SkinType
   skinNumber: string
 }
-export type GetDemageSkinResponse = {
+export type GetDamageSkinResponse = {
   children: string[]
   type: number
   value: string
