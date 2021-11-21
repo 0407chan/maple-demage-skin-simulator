54fb2e26b762c151b0462eed728bbf52449322f9
이찬호 <0407chan@naver.com>
2021-11-21 23:46:27 +0900

    skinId->skinNumber

--- src/type/damage-skin.ts
+++ src/type/damage-skin.ts
@@ -1,11 +1,18 @@
 export type SkinType = 'NoCri0' | 'NoCri1' | 'NoRed0' | 'NoRed1'
 export type GetDamageSkinQuery = {
-  skinId?: number
+  skinNumber?: number
   skinType: SkinType
-  skinNumber: string
+  damageNumber: string
 }
 export type GetDamageSkinResponse = {
   children: string[]
   type: number
   value: string
+}
+
+export type DamageType = {
+  id: string
+  skinNumber: number
+  isCritical?: boolean
+  damage: number
 }
