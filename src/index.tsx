bd79a83c19fcb515d4968bdfb47f7e195c805e71
이찬호 <0407chan@naver.com>
2021-11-23 00:08:20 +0900

    root에 리코일 적용

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,18 @@ import 'antd/dist/antd.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import { RecoilRoot } from 'recoil'
 import App from './App'
 import './styles/globals.scss'
+
 const queryClient = new QueryClient()
 
 ReactDOM.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RecoilRoot>
+        <App />
+      </RecoilRoot>
     </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root')
