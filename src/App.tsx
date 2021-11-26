c04ca31cc35ef4070fc1b18be80f98877a99c007
이찬호 <0407chan@naver.com>
2021-11-27 01:08:11 +0900

    ga 다시 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from 'react'
-import ReactGA from 'react-ga'
+import React, { useState } from 'react'
 import { v4 as uuid } from 'uuid'
 import * as S from './appStyle'
 import DamageSkin from './components/DamageSkin'
@@ -55,14 +54,6 @@ const App: React.FC = () => {
     max = Math.floor(max)
     return Math.floor(Math.random() * (max - min)) + min //최댓값은 제외, 최솟값은 포함
   }
-
-  const initReactGA = () => {
-    ReactGA.initialize('UA-178457189-3')
-  }
-
-  useEffect(() => {
-    initReactGA()
-  }, [])
 
   return (
     <S.Container>
