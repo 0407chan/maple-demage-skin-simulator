ef25e0270e3712eb84a6c1aef2aea673dfc88427
이찬호 <0407chan@naver.com>
2021-11-26 23:12:30 +0900

    [8] 유닛 뎀스 적용

--- src/components/DamageSkin/DamageSkin.tsx
+++ src/components/DamageSkin/DamageSkin.tsx
@@ -1,13 +1,18 @@
-import { DamageType } from '@/type/damage-skin'
+import { DamageType, ItemDto } from '@/type/damage-skin'
 import React, { useEffect, useState } from 'react'
 import * as S from './style'
 
 type Props = {
   damageItem: DamageType
   setDamageList: React.Dispatch<React.SetStateAction<DamageType[]>>
+  currentSkin?: ItemDto
 }
-const DamageSkin: React.FC<Props> = ({ damageItem, setDamageList }) => {
-  const [timer, setTimer] = useState<number>(1000)
+const DamageSkin: React.FC<Props> = ({
+  damageItem,
+  setDamageList,
+  currentSkin
+}) => {
+  const [timer] = useState<number>(1000)
   const [visible, setVisible] = useState<boolean>(true)
 
   const getSkin1Image = (num: number) => {
@@ -25,6 +30,24 @@ const DamageSkin: React.FC<Props> = ({ damageItem, setDamageList }) => {
     }
   }
 
+  const isUnit = () => currentSkin?.name.includes('유닛')
+
+  const getUnit = (unit: '만' | '억') => {
+    if (damageItem.isCritical) {
+      if (unit === '만') {
+        return `${process.env.PUBLIC_URL}/images/export/Effect-DamageSkin.img-${damageItem.skinNumber}-NoCustom-NoCri1-3.png` // 만
+      } else {
+        return `${process.env.PUBLIC_URL}/images/export/Effect-DamageSkin.img-${damageItem.skinNumber}-NoCustom-NoCri1-4.png` // 억
+      }
+    } else {
+      if (unit === '만') {
+        return `${process.env.PUBLIC_URL}/images/export/Effect-DamageSkin.img-${damageItem.skinNumber}-NoCustom-NoRed1-3.png`
+      } else {
+        return `${process.env.PUBLIC_URL}/images/export/Effect-DamageSkin.img-${damageItem.skinNumber}-NoCustom-NoRed1-4.png`
+      }
+    }
+  }
+
   const getCriticalImage = () => {
     return `${process.env.PUBLIC_URL}/images/export/Effect-DamageSkin.img-${damageItem.skinNumber}-NoCri1-effect3.png`
   }
@@ -38,6 +61,32 @@ const DamageSkin: React.FC<Props> = ({ damageItem, setDamageList }) => {
     }, timer)
   }, [])
 
+  const getDamageString = () => {
+    if (isUnit()) {
+      const numString = `${damageItem.damage}`
+      let result = ''
+      if (numString.length <= 12 && numString.length > 8) {
+        result += `${numString.slice(-12, -8)}억`
+        if (Number(numString.slice(-8, -4)) > 0) {
+          result += `${numString.slice(-8, -4)}만`
+        }
+        if (Number(numString.slice(-4)) > 0) {
+          result += `${numString.slice(-4)}`
+        }
+      }
+      // 만
+      else if (numString.length <= 8 && numString.length > 4) {
+        result += `${numString.slice(-8, -4)}만`
+        if (Number(numString.slice(-4)) > 0) {
+          result += `${numString.slice(-4)}`
+        }
+      }
+      return result
+    } else {
+      return `${damageItem.damage}`
+    }
+  }
+
   if (!visible) return null
 
   return (
@@ -47,25 +96,29 @@ const DamageSkin: React.FC<Props> = ({ damageItem, setDamageList }) => {
           <img draggable={false} alt="critical-img" src={getCriticalImage()} />
         </S.CriEffect>
       )}
-      {`${damageItem.damage}`.split('').map((num, index) => (
-        <img
-          key={index}
-          style={{
-            width: 'fit-content',
-            height: 'fit-content',
-            zIndex: index + 1,
-            marginBottom: index % 2 === 0 ? 4 : 0,
-            marginTop: index % 2 === 1 ? 4 : 0
-          }}
-          draggable={false}
-          alt={`skin-img-${num}-${index}`}
-          src={
-            index === 0
-              ? getSkin1Image(Number(num))
-              : getSkin0Image(Number(num))
-          }
-        />
-      ))}
+      {getDamageString()
+        .split('')
+        .map((num, index) => (
+          <img
+            key={index}
+            style={{
+              width: 'fit-content',
+              height: 'fit-content',
+              zIndex: index + 1,
+              marginBottom: index % 2 === 0 ? 4 : 0,
+              marginTop: index % 2 === 1 ? 4 : 0
+            }}
+            draggable={false}
+            alt={`skin-img-${num}-${index}`}
+            src={
+              num === '만' || num === '억'
+                ? getUnit(num as '만' | '억')
+                : index === 0
+                ? getSkin1Image(Number(num))
+                : getSkin0Image(Number(num))
+            }
+          />
+        ))}
     </S.Container>
   )
 }
