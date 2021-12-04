c02ac1ef5349bbdb2e647cd054c4569ea3aa2e2d
이찬호 <0407chan@naver.com>
2021-12-04 14:56:00 +0900

    remove. recoil

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import 'antd/dist/antd.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { QueryClient, QueryClientProvider } from 'react-query'
-import { RecoilRoot } from 'recoil'
 import App from './App'
 import './styles/globals.scss'
 
@@ -11,9 +10,7 @@ const queryClient = new QueryClient()
 ReactDOM.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <RecoilRoot>
-        <App />
-      </RecoilRoot>
+      <App />
     </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root')
