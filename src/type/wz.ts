887adc069f922d6c36aafd282983284d0e8c5990
이찬호 <0407chan@naver.com>
2021-12-08 23:46:48 +0900

    [17] RegionType추가

--- src/type/wz.ts
+++ src/type/wz.ts
@@ -1,6 +1,18 @@
+export type RegionType =
+  | 'EMS'
+  | 'KMST'
+  | 'GMS'
+  | 'THMS'
+  | 'TWMS'
+  | 'CMS'
+  | 'KMS'
+  | 'TMS'
+  | 'CMST'
+  | 'SEA'
+  | 'JMS'
 export type WzType = {
   hasImages: boolean
   isReady: boolean
   mapleVersionId: string
-  region: string
+  region: RegionType
 }
