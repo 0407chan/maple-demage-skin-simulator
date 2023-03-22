7dc5d6439d0f77d2bd2ef2cf91127575f1ffff57
이찬호 <0407chan@naver.com>
2023-03-22 21:51:20 +0900

    vite 설치

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+import checker from 'vite-plugin-checker'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+export default defineConfig({
+  plugins: [react(), checker({ typescript: true }), tsconfigPaths()],
+  server: {
+    port: 3000
+  },
+  build: {
+    manifest: true
+  }
+})
